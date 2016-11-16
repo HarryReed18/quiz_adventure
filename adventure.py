67348c5a65ff2bbe257e37d4dff2ce5ffa5308fd
harry reed <hreed18@VenerableBede.schools.sunderland.gov.uk>
2016-11-16 12:17:57 +0000

    To do at home

--- adventure.py
+++ adventure.py
@@ -1,8 +1,70 @@
 # An adventure game
+from time import sleep
+health = 0
+enemy_health = 0
+karma = 0
+def into():
+
+     
+     global health
+     global enemy_health
+     global karma
+    
+   
 
 def room1():
 
-    ... make a game
+    
+    global health
+    global enemy_health
+    global karma
+
+    print("You wake up, lost and alone in a dark moving prison with no memory of how you got there")
+    sleep(2)
+    print("your memory is blurry and the only thing you remember is a woman screaming at you as you're being pulled away")
+    sleep(1)
+    print("Wait...")
+    sleep(3)
+    print("Who was it")
+
+    print("Contemplating your life and waiting for the prison to stop,  you suddenly hear a horrific roar emerge from underneath a dusty grey cloth") 
+    print("What do you do?")
+    sleep(1)
+    print("A, Quench your curiosity and remove the cloth")
+    sleep(1)
+    print("B. Be a man and kill the beast!!")
+    sleep(1)
+    answer = input("???????- ")
+
+    if answer.lower() == "a":
+        print("You lift the cloth to find that the source of the creature is simply just as terrified as  you and is squealing in terror")
+        sleep(2)
+        print("You feel symapthy for the creature and calm it down")
+        sleep(1)
+        print("After looking around for a short while you find some stale bread and share it with the creature")
+        karma = karma + 10
+        print("Karma: ", karma )
+
+    elif answer.lower() == "b":
+        print("You see a lighter in the corner of your eye and set the suprisingly flamable cloth on fire")
+        sleep(1)
+        print("You curl up in a ball in the center of the floor and cry as the creature cries out in agony as it slowly burns to death")
+        print("as it burns you cant help but begin to feel hungry...")
+        print(karma = karma - 5)
+        sleep(5)
+
+        room2()
+
+def room2():
+    
+    global health
+    global enemy_health
+    global karma
+    
+    
+    
+        
+        
 
 
 
--- quiz.py
+++ quiz.py
@@ -1,8 +1,102 @@
 # Our quiz!
+from time import sleep
+score = 0
+name = ""
 
 def quiz():
+    global name
+    
+    print("WHAT IS YOUR NAME")
+    name = input("Type Name Here: ")
+    print(" Why Hello there " + name + " , do wanna play a game...")
 
-    ... make a quiz!
+    print("")
+
+    sleep(2)
+
+    print("...")
+
+    sleep(3)
+
+    print(".......")
+    
+
+    sleep(5)
+
+    print("I'll take that as a yes")
+    question1()
+
+def question1():
+    global score
+
+    print("Who's the best, Bilbo or Frodo?")
+    print("A. Bilbo")
+    print("B. Frodo")
+    answer = input("Choose one or die!")
+
+
+    if answer.lower() == "a":
+        score = score + 100
+        print("The Hobbit Huh??")
+        print(score)
+
+    elif answer.lower() == "b":
+        score = score + 200
+        print("Yeah, Lord of The Rings is much better")
+        print(score)
+        question2()
+
+def question2():
+    global score
+
+    print("Question 2 " + name + ", you ready?")
+    sleep(1)
+
+    print("In which movie is the name of the main character an anogram for ONE?")
+    print("A. Lucy")
+    print("B. The Matrix")
+    print("C. War of The Worlds")
+    answer2 = input("Guess")
+
+    if answer2.lower() == "a":
+        print("How is LUCY an anogram of ONE")
+        print(score = score - 100)
+
+    elif answer2.lower() == "b":
+        print("Correct, NEO is an anogram for ONE")
+        print(score = score + 200)
+
+    elif answer2.lower() == "c":
+        sleep(2)
+        print("You dont even know his name do you?")
+        answer_extra = input("YES OR NO!?")
+
+        if answer_extra.lower() == "yes":
+            print("oh,")
+            sleep(1)
+            print("Brownie points for you then")
+            print(score = score + 50)
+
+        elif answer_extra.lower() == "no":
+            print("I KNEW IT!!!")
+            print("NO MORE POINTS FOR YOU!")
+            print(score = score - score)
+            print("You have upset me.")
+            sleep(1)
+            quit()
+
+def final_screen():
+    print("This is all there is " + name)
+    print("Good Bye for now")
+    print("We'll be back soon")
+
+        
+            
+        
+
+        
+        
+
 
 
 
